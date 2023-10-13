80afe7b4ac458c5a3d49119a7eba2f7b0ea196c4
Vijay <vijay.thorat@biddano.com>
2023-10-13 16:18:36 +0530

    changed app name

--- quizeapp/scripts/nginx.sh
+++ quizeapp/scripts/nginx.sh
@@ -4,9 +4,9 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/django_project/nginx/nginx.conf /etc/nginx/sites-available/blog
-sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled/
-#sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+sudo cp /home/ubuntu/django_project/nginx/nginx.conf /etc/nginx/sites-available/quizeapp
+sudo ln -s /etc/nginx/sites-available/quizeapp /etc/nginx/sites-enabled/
+#sudo ln -s /etc/nginx/sites-available/quizeapp /etc/nginx/sites-enabled
 #sudo nginx -t
 sudo gpasswd -a www-data ubuntu
 sudo systemctl restart nginx
